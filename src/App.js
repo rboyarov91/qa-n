d1a6c3de64bd9dad06548427cfeeff2b404ee0b3
Roman <rboyarov91@gmail.com>
2017-08-21 09:46:37 -0400

    Create basic html to test and add new script to package.json

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+	render() {
+		return (<html>
+					<header><title>This is title</title></header>
+					<body>
+					Hello world
+					</body>
+				</html>);
+	}
+}
+
+export default App;
