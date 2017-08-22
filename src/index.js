be670c0a500310ec4c95d80abf7a4ba97d9bc0d9
Roman <rboyarov91@gmail.com>
2017-08-22 11:38:19 -0400

    Add routing to app

--- src/index.js
+++ src/index.js
@@ -1,7 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import App from './App';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+ReactDOM.render(
+	<Router>
+  		<div>
+  			<Route path='/' component={App} />
+    		<Route path='/home' component={App} />
+    		<Route path='/test' component={App} />
+  		</div>
+	</Router>, 
+    document.getElementById('root')
+	);
 registerServiceWorker();
