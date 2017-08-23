56905a060ceed0478cf001e7f8c4bfe00be1c544
Roman <rboyarov91@gmail.com>
2017-08-23 13:51:41 -0400

    Make table render passed in generic json data

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -25,8 +25,9 @@ class Table extends Component {
 	}
 
 	componentDidMount() {
-		let testData = '[{ "name":"John", "age":30, "city":"New York"},{ "name":"Ben", "age":24, "city":"New Jersey"}]'
-		let parsedData = JSON.parse(testData)
+		let testData = '[{"date":"May 13, 2016","Loadtime":" 7.72s","TTFB":" 0.253s","Start Render":" 1.505s","Visually Complete":" 10.199s","Speed Index":" 3364","DOM Elements":" 694","DOCTime":" 7.72s","DOCRequests":" 85","DOCBytesIn":" 3852 KB","FLTime":" 8.298s","FLRequests":" 98","FLBytesIn":" 3914 KB","200s":" 91","404s":" 0","Others":" 8","Site Search":"6,290","PageScore":" 59"},\
+		{"date":"June 16, 2017","Loadtime":" 12.4s","TTFB":" 0.123s","Start Render":" 2.01s","Visually Complete":" 13.438s","Speed Index":" 4281","DOM Elements":" 703","DOCTime":" 6.98s","DOCRequests":" 102","DOCBytesIn":" 3987 KB","FLTime":" 7.32s","FLRequests":" 90","FLBytesIn":" 3213 KB","200s":" 104","404s":" 4","Others":" 5","Site Search":"6,290","PageScore":" 59"}]';
+		let parsedData = JSON.parse(this.props.data)
 		let parsedHeadings;
 		if (parsedData instanceof Array) {
 			parsedHeadings = Object.keys(parsedData[0])
@@ -34,17 +35,26 @@ class Table extends Component {
 			parsedHeadings = Object.keys(parsedData)
 		}
 		this.setState({
-			headings : parsedHeadings,
-			data : parsedData
+			headings : parsedHeadings
 		})
+		if (parsedData instanceof Array) {
+			this.setState({
+				data : parsedData
+			})
+		} else {
+			this.setState({
+				data : [parsedData]
+			})
+		}
+
 	}
 	render() {
 		return (
 			<div>
-				<table className="SimpleTable">
+				<table className="Table">
 					<thead>
 						<tr>
-							{this.state.headings.map((heading) => <th>{heading}</th>)}
+							{this.state.headings.map((heading) => <th scope="col">{heading}</th>)}
 						</tr>
 					</thead>
 					<tbody>
