30a876043201bc15d8c7dd539fd9da94013c71a3
Roman <rboyarov91@gmail.com>
2017-08-25 11:28:33 -0400

    Grab the test data using axios in TabContainer component

--- src/metrics/TabContainer.js
+++ src/metrics/TabContainer.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
 import GraphView from './GraphView';
 import TableView from './TableView';
+import axios from 'axios';
+
+const BASE_URL = 'http://localhost:3001';
 
 class TabContainer extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			graphActive:true
+			graphActive:true,
+			siteData:null
 		};
 	}
 
@@ -18,26 +22,24 @@ class TabContainer extends Component {
 		})
 	}
 
-	render() {
-		let testData = '[{"date":"May 13, 2016","Loadtime":" 7.72s","TTFB":" 0.253s","Start Render":" 1.505s","Visually Complete":" 10.199s","Speed Index":" 3364","DOM Elements":" 694",\
-		"DOCTime":" 7.72s","DOCRequests":" 85","DOCBytesIn":" 3852 KB","FLTime":" 8.298s","FLRequests":" 98","FLBytesIn":" 3914 KB","200s":" 91","404s":" 0","Others":" 8","Site Search":"6,290","PageScore":" 59"},\
-		{"date":"June 16, 2017","Loadtime":" 12.4s","TTFB":" 0.123s","Start Render":" 2.01s","Visually Complete":" 13.438s","Speed Index":" 4281","DOM Elements":" 703","DOCTime":" 6.98s",\
-		"DOCRequests":" 102","DOCBytesIn":" 3987 KB","FLTime":" 7.32s","FLRequests":" 90","FLBytesIn":" 3213 KB","200s":" 104","404s":" 4","Others":" 5","Site Search":"6,290","PageScore":" 64"},\
-		{"date":"June 16, 2017","Loadtime":" 12.4s","TTFB":" 0.123s","Start Render":" 2.01s","Visually Complete":" 13.438s","Speed Index":" 4281","DOM Elements":" 703","DOCTime":" 6.98s",\
-		"DOCRequests":" 102","DOCBytesIn":" 3987 KB","FLTime":" 7.32s","FLRequests":" 90","FLBytesIn":" 3213 KB","200s":" 104","404s":" 4","Others":" 5","Site Search":"6,290","PageScore":" 59"},\
-		{"date":"June 16, 2017","Loadtime":" 12.4s","TTFB":" 0.123s","Start Render":" 2.01s","Visually Complete":" 13.438s","Speed Index":" 4281","DOM Elements":" 703","DOCTime":" 6.98s",\
-		"DOCRequests":" 102","DOCBytesIn":" 3987 KB","FLTime":" 7.32s","FLRequests":" 90","FLBytesIn":" 3213 KB","200s":" 104","404s":" 4","Others":" 5","Site Search":"6,290","PageScore":" 58"},\
-		{"date":"June 16, 2017","Loadtime":" 12.4s","TTFB":" 0.123s","Start Render":" 2.01s","Visually Complete":" 13.438s","Speed Index":" 4281","DOM Elements":" 703","DOCTime":" 6.98s",\
-		"DOCRequests":" 102","DOCBytesIn":" 3987 KB","FLTime":" 7.32s","FLRequests":" 90","FLBytesIn":" 3213 KB","200s":" 104","404s":" 4","Others":" 5","Site Search":"6,290","PageScore":" 65"},\
-		{"date":"June 16, 2017","Loadtime":" 12.4s","TTFB":" 0.123s","Start Render":" 2.01s","Visually Complete":" 13.438s","Speed Index":" 4281","DOM Elements":" 703","DOCTime":" 6.98s",\
-		"DOCRequests":" 102","DOCBytesIn":" 3987 KB","FLTime":" 7.32s","FLRequests":" 90","FLBytesIn":" 3213 KB","200s":" 104","404s":" 4","Others":" 5","Site Search":"6,290","PageScore":" 70"},\
-		{"date":"June 16, 2017","Loadtime":" 12.4s","TTFB":" 0.123s","Start Render":" 2.01s","Visually Complete":" 13.438s","Speed Index":" 4281","DOM Elements":" 703","DOCTime":" 6.98s",\
-		"DOCRequests":" 102","DOCBytesIn":" 3987 KB","FLTime":" 7.32s","FLRequests":" 90","FLBytesIn":" 3213 KB","200s":" 104","404s":" 4","Others":" 5","Site Search":"6,290","PageScore":" 59"},\
-		{"date":"June 16, 2017","Loadtime":" 12.4s","TTFB":" 0.123s","Start Render":" 2.01s","Visually Complete":" 13.438s","Speed Index":" 4281","DOM Elements":" 703","DOCTime":" 6.98s",\
-		"DOCRequests":" 102","DOCBytesIn":" 3987 KB","FLTime":" 7.32s","FLRequests":" 90","FLBytesIn":" 3213 KB","200s":" 104","404s":" 4","Others":" 5","Site Search":"6,290","PageScore":" 43"},\
-		{"date":"June 16, 2017","Loadtime":" 12.4s","TTFB":" 0.123s","Start Render":" 2.01s","Visually Complete":" 13.438s","Speed Index":" 4281","DOM Elements":" 703","DOCTime":" 6.98s",\
-		"DOCRequests":" 102","DOCBytesIn":" 3987 KB","FLTime":" 7.32s","FLRequests":" 90","FLBytesIn":" 3213 KB","200s":" 104","404s":" 4","Others":" 5","Site Search":"6,290","PageScore":" 62"}]';
+	getSiteData = () => {
+		const url = BASE_URL + '/api/siteInfo'
+		return axios.get(url).then(response => response.data);
+	}
 
+	componentDidMount() {
+		const url = BASE_URL + '/api/siteInfo'
+		//console.log("Setting state to: " + this.getSiteData())
+		this.getSiteData().then((data) => {
+			this.setState({siteData:data})
+		})
+		
+		//axios.get(url).then(response => this.setState({siteData:response.data}))
+	}
+
+	render() {
+		let testData = this.state.siteData
+		//console.log("data being used: " + testData)
 		return (
 			<div className={TabContainer.name}>
         <div className="row">
@@ -47,7 +49,6 @@ class TabContainer extends Component {
                     <li role="presentation" data="graph" className={'tab' + (this.state.graphActive ? ' active' : '')} onClick={this.handleOnClick}><a href="#graph" aria-controls="home" role="tab" data-toggle="tab">Graphs</a></li>
                     <li role="presentation" data="data" className={'tab' + (!this.state.graphActive ? ' active' : '')} onClick={this.handleOnClick}><a href="#data" aria-controls="profile" role="tab" data-toggle="tab">Data</a></li>
                 </ul>
-
                
                 <div className="tab-content">
                     <div role="tabpanel" className={'tab-pane' + (this.state.graphActive ? ' active' : '')} id="graph">
