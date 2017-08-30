97775cecf74e66280ebc6795e1005535e606c002
Roman <rboyarov91@gmail.com>
2017-08-30 11:43:21 -0400

    Refactor to use gulp for running backend, watch for changes and recompile. Still need to reload browser to see changes

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,20 +1,45 @@
-import gulp from 'gulp';
-import less from 'gulp-less';
-import cssmin from 'gulp-cssmin';
-import rename from'gulp-rename';
+import gulp from "gulp"
+import plumber from "gulp-plumber"
+import babel from "gulp-babel"
+import cache from "gulp-cached"
+import eslint from "gulp-eslint"
+import nodemon from "gulp-nodemon"
+import path from "path"
 
-  gulp.task('less', function () {
+const SOURCES = ["src/backend/**/*.js"];
+const DEST_DIR = "dist/src/backend";
+const MAIN = path.join(DEST_DIR, "server.js");
 
-  return gulp.src('./src/All.less')
-    .pipe(less().on('error', function (err) {
-      console.log(err);
+gulp.task("babelify", function(){
+    gulp.src(SOURCES)
+    // Use plumber to handle errors without terminating the pipe-chain
+    .pipe(plumber())
+    // Use gulp-cache to avoid re-transpiling files with no changes
+    .pipe(cache("babelify-cache"))
+    .pipe(babel({
+        presets: ["es2015"],
+        plugins: ["transform-class-properties"]
     }))
-    .pipe(cssmin().on('error', function(err) {
-      console.log(err);
-    }))
-    .pipe(rename({suffix: '.min'}))
-    .pipe(gulp.dest('./public'));
-
+    .pipe(gulp.dest(DEST_DIR));
 });
 
-  gulp.task('default', ['less']);
+// TODO add linting to project
+/*gulp.task("lint", function(){
+    gulp.src(SOURCES)
+    .pipe(eslint())
+    .pipe(eslint.format())
+    .pipe(eslint.failAfterError());
+});*/
+
+gulp.task("watch", function(){
+    gulp.watch(SOURCES, ["babelify"]);
+});
+
+gulp.task("nodemon", function(){
+    nodemon({
+        script: MAIN,
+        args:process.argv.splice(3, process.argv.length)
+    });
+});
+
+gulp.task("dev", ["babelify", "watch", "nodemon"]);
